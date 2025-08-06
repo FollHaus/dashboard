f4a0460cd6cb087a805c0f311de0244b5466863a
Sergeyi <74300432+FollHaus@users.noreply.github.com>
2025-08-06 20:01:25 +0300

    feat: register category controller (#8)

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { CategoryModel } from './category.model'
 import { CategoryService } from './category.service'
+import { CategoryController } from './category.controller'
 
 @Module({
 	imports: [
 		SequelizeModule.forFeature([CategoryModel]) // регистрируем CategoryModel
 	],
+	controllers: [CategoryController],
 	providers: [CategoryService],
 	exports: [
 		CategoryService, // если кому-то нужен сервис
