b3521360b4925b4d49ba90f0a057d7c66a6d2ff2
Sergeyi <74300432+FollHaus@users.noreply.github.com>
2025-08-06 19:09:40 +0300

    refactor: remove user link from tasks

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,7 +1,49 @@
-import { Controller } from '@nestjs/common';
-import { TaskService } from './task.service';
+import {
+        Body,
+        Controller,
+        Delete,
+        Get,
+        Param,
+        ParseIntPipe,
+        Post,
+        Put
+} from '@nestjs/common'
+import { TaskService } from './task.service'
+import { TaskModel } from './task.model'
+import { CreateTaskDto } from './dto/task.dto'
+import { UpdateTaskDto } from './dto/update.task.dto'
 
 @Controller('task')
 export class TaskController {
-  constructor(private readonly taskService: TaskService) {}
+        constructor(private readonly taskService: TaskService) {}
+
+        @Post()
+        async create(@Body() dto: CreateTaskDto): Promise<TaskModel> {
+                return this.taskService.create(dto)
+        }
+
+        @Get()
+        async findAll(): Promise<TaskModel[]> {
+                return this.taskService.findAll()
+        }
+
+        @Get(':id')
+        async findOne(
+                @Param('id', ParseIntPipe) id: number
+        ): Promise<TaskModel> {
+                return this.taskService.findOne(id)
+        }
+
+        @Put(':id')
+        async update(
+                @Param('id', ParseIntPipe) id: number,
+                @Body() dto: UpdateTaskDto
+        ): Promise<TaskModel> {
+                return this.taskService.update(id, dto)
+        }
+
+        @Delete(':id')
+        async remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
+                return this.taskService.remove(id)
+        }
 }
