9e93d2626a64833413257dceba8aa503cfdf0ed3
Sergeyi <74300432+FollHaus@users.noreply.github.com>
2025-08-07 09:58:33 +0300

    refactor: remove unnecessary await in JWT config (#15)

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -5,8 +5,8 @@ import { JwtModuleOptions } from '@nestjs/jwt'
  * Функция `getJWTConfig` возвращает конфигурацию для модуля JWT.
  * Использует секретный ключ из переменной окружения `JWT_SECRET`.
  */
-export const getJWTConfig = async (
-	configService: ConfigService
-): Promise<JwtModuleOptions> => ({
-	secret: await configService.get('JWT_SECRET') // Получаем секретный ключ из конфига
+export const getJWTConfig = (
+        configService: ConfigService
+): JwtModuleOptions => ({
+        secret: configService.get('JWT_SECRET') // Получаем секретный ключ из конфига
 })
